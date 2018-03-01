0e93254f5904dd6e8c8704f1e252877e0d45a63f
tuanlestudy <tuanlestudy@gmail.com>
2018-03-01 00:34:28 -0800

    edit update.py

--- update.py
+++ update.py
@@ -1,11 +1,13 @@
-import subprocess
+import os
+
 sudo_install = 'sudo apt-get install '
 sudo = 'sudo apt '
-result = subprocess.run([sudo_install + 'update', '-y'], stdout=subprocess.PIPE)
-result.stdout
-result = subprocess.run([sudo_install + 'upgrade', '-y'], stdout=subprocess.PIPE)
-result.stdout
-result = subprocess.run([sudo + 'autoremove', '-y'], stdout=subprocess.PIPE)
-result.stdout
-result = subprocess.run([sudo + 'autoclean', '-y'], stdout=subprocess.PIPE)
-result.stdout
+
+print(sudo_install+'update -y')
+os.system(sudo_install+'update -y')
+print(sudo_install+'upgrade -y')
+os.system(sudo_install+'upgrade -y')
+print('sudo + 'autoremove -y'')
+os.system(sudo + 'autoremove -y')
+print(sudo + 'autoclean -y')
+os.system(sudo + 'autoclean -y')
